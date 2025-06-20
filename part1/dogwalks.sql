d8ab4c70613cd03fdd0191691f4093f92acf28ff
taishicoding <a1904976@adelaide.edu.au>
2025-06-20 15:44:53 +0000

    [automatic commit] 2025-06-20 15:44:53 UTC 180.150.112.22

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -69,3 +69,5 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
  ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Donko', 'small'),
  ((SELECT user_id FROM Users WHERE username = 'ken123'), 'Sakura', 'medium'),
  ((SELECT user_id FROM Users WHERE username = 'ken123'), 'Sasha', 'large');
+
+ INSERT INTO WalkRequests (dog_id, re)
