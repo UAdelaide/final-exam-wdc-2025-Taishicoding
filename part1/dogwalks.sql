36c99e4a5984f0bbfc8549a7efde79b2b5f3c59c
taishicoding <a1904976@adelaide.edu.au>
2025-06-20 15:35:02 +0000

    [automatic commit] 2025-06-20 15:35:02 UTC 180.150.112.22

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -58,5 +58,5 @@ CREATE TABLE WalkRatings (
 
 INSERT INTO Users (username, email, password_hash, role) VALUES
  ('alice123', 'alice@example.com', 'hashed123', 'owner'),
- ('bobwalker', 'bob@example.com, password hash hashed456, and role walker),
- ('carol123', carol@example.com, password hash hashed789, and role owner),
+ ('bobwalker', 'bob@example.com', 'password hash 'hashed456, and role walker),
+ ('carol123', 'carol@example.com', 'hashed789, and role owner),
