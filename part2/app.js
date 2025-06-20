0244237555121b507abc88d76fa6b7fe4cd03977
taishicoding <a1904976@adelaide.edu.au>
2025-06-20 20:49:01 +0000

    [automatic commit] 2025-06-20 20:49:01 UTC 180.150.112.22

--- part2/app.js
+++ part2/app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Serving HTML files
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.get('/owner-dashboard.html', (req,res) => {
