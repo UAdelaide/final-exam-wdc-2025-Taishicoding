6c90745185d2d361b50a829edd42f81d5b2c3cc8
taishicoding <a1904976@adelaide.edu.au>
2025-06-20 20:42:39 +0000

    [automatic commit] 2025-06-20 20:42:39 UTC 180.150.112.22

--- part2/app.js
+++ part2/app.js
@@ -86,6 +86,7 @@ app.post('/api/users/login', async (req, res) => {
     console.error('Login Error');
 }
 });
+// logout
 app.post('/api/users/logout', (req, res) => {
     req.session.destroy((err) => {
         if (err) {
