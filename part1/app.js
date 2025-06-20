5ed519ac75eda1243922b638c7693f9524a78970
taishicoding <a1904976@adelaide.edu.au>
2025-06-20 17:01:44 +0000

    [automatic commit] 2025-06-20 17:01:44 UTC 180.150.112.22

--- part1/app.js
+++ part1/app.js
@@ -103,7 +103,7 @@ app.get('/api/dogs', async (req, res) => {
 // Route to return all walk requests
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [requests] = await db.execute('SELECT wr.request_id, d.name as dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username as owner_username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id WHERE wr.status = 'open' ORDER );
+    const [requests] = await db.execute('SELECT wr.request_id, d.name as dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username as owner_username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id WHERE wr.status = 'open' ORDER BY wr.requested_time);
     res.json(requests);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch open walk requests' });
