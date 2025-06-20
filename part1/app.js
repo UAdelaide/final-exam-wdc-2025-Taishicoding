c98ec58e9d110b90f1d2b4b0420c34d0d6bb0331
taishicoding <a1904976@adelaide.edu.au>
2025-06-20 16:28:28 +0000

    [automatic commit] 2025-06-20 16:28:28 UTC 180.150.112.22

--- part1/app.js
+++ part1/app.js
@@ -77,7 +77,7 @@ let db;
       `);
       await db.execute(`
         INSERT INTO WalkApplications (request_id, walker_id, status) VALUES
-        ((SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs Where name = )), 'Donko', 'small'),
+        ((SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs Where name = 'Bella')), 'Donko', 'small'),
       `);
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
