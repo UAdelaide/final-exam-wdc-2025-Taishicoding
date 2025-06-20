836b8b0c079edc88f4be552ce731856792d940c5
taishicoding <a1904976@adelaide.edu.au>
2025-06-20 17:50:51 +0000

    [automatic commit] 2025-06-20 17:50:51 UTC 180.150.112.22

--- part1/app.js
+++ part1/app.js
@@ -124,7 +124,7 @@ app.get('/api/walkers/summary', async (req, res) => {
     res.status(500).json({ error: 'Failed to walker summary' });
   }
 });
-app.get
+app.get('/',(req,res)=>)
 app.use(express.static(path.join(__dirname, 'public')));
 module.exports = app;
 app.listen(3000, ()=>{
