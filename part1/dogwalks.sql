fa6ece1817c3d8630baf2d23ea3b4cbfde827d27
taishicoding <a1904976@adelaide.edu.au>
2025-06-20 15:47:06 +0000

    [automatic commit] 2025-06-20 15:47:06 UTC 180.150.112.22

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -72,4 +72,4 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 
  INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
  ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123'))
- )
+ '2025-06-10 08:00:00')
